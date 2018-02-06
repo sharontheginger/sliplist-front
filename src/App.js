0d59ba87ac0042a5e27266f92ff7723a8b1bf782
Sharon <sharontheginger@me.com>
2018-02-06 13:51:34 -0800

    basic navbar added

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import {
 } from 'react-bootstrap'
 import Locations from './Pages/Locations'
 import Newuser from './Pages/Newuser'
+import NavbarTop from './components/NavbarTop.js'
 
 
 class App extends Component {
@@ -22,41 +23,7 @@ class App extends Component {
     return (
       <Router>
         <div>
-		<div>
-          <Navbar inverse collapseOnSelect>
-		        <Navbar.Header>
-			        <Navbar.Brand>
-				        <a href="#brand">slip list</a>
-			        </Navbar.Brand>
-			        <Navbar.Toggle />
-		        </Navbar.Header>
-		        <Navbar.Collapse>
-			        <Nav>
-				        <NavItem eventKey={1} href="#">
-					        Link
-				        </NavItem>
-				        <NavItem eventKey={2} href="#">
-					        Link
-				        </NavItem>
-				        <NavDropdown eventKey={3} title="Dropdown" id="basic-nav-dropdown">
-					        <MenuItem eventKey={3.1}>Action</MenuItem>
-					        <MenuItem eventKey={3.2}>Another action</MenuItem>
-					        <MenuItem eventKey={3.3}>Something else here</MenuItem>
-					        <MenuItem divider />
-					        <MenuItem eventKey={3.3}>Separated link</MenuItem>
-				        </NavDropdown>
-			        </Nav>
-			        <Nav pullRight>
-				        <NavItem eventKey={1} href="#">
-					        Link Right
-				        </NavItem>
-				        <NavItem eventKey={2} href="#">
-					        Link Right
-				        </NavItem>
-			        </Nav>
-		        </Navbar.Collapse>
-	        </Navbar>
-        </div>
+					<NavbarTop />
 		<div>
         <Carousel>
 		<Carousel.Item>

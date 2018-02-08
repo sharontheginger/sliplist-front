c4194600596a3ab6415e0db0543e4eff43174ede
learn <learn@learn06-2.local>
2018-02-08 14:14:20 -0800

    availabilities tests update

--- src/__tests__/Availabilities_test.js
+++ src/__tests__/Availabilities_test.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom'
 import Availabilities from '../pages/Availabilities'
 import { mount } from 'enzyme'
@@ -18,4 +18,16 @@ it('Renders the Availabilities firstName', ()=>{
   const component = mount(<Availabilities />)
   const firstName = component.find('ul > .availabilities-firstName').first()
   expect(firstName.text()).toBe("fernando")
+})
+
+it('Renders the Availabilities lastName', ()=>{
+  const component = mount(<Availabilities availabilities={availabilities} />)
+  const age = component.find('ul > .availabilities-lastName').first()
+  expect(lastName.text()).toBe("fonzu")
+})
+
+it('Renders Availabilities email', ()=>{
+  const component = mount(<Availabilities availabilities={availabilities} />)
+  const age = component.find('.availabilities-email').first()
+  expect(email.text()).toBe("mandrid@yahoo.com")
 })

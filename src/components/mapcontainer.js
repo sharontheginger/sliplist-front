859eec92390718c500d8d5033111feabaaeb8d5d
Luis Gutierrez <spartanxlg@gmail.com>
2018-02-20 16:17:09 -0800

    updated more style @4:17pm

--- src/components/mapcontainer.js
+++ src/components/mapcontainer.js
@@ -3,69 +3,71 @@ import {Map, Marker, GoogleApiWrapper} from 'google-maps-react';
 
 export class MapContainer extends Component {
 	constructor(props) {
-    super(props);
-    this.state = {
-		availabilities:[],
-      showingInfoWindow: false,
-      activeMarker: {},
-      selectedPlace: {},
-    }
-	this.onMarkerClick = this.onMarkerClick.bind(this);
- this.onMapClicked = this.onMapClicked.bind(this);
-}
+		super(props);
+		this.state = {
+			availabilities:[],
+			showingInfoWindow: false,
+			activeMarker: {},
+			selectedPlace: {},
+		}
 
-onMarkerClick = (props, marker, e)=>{
-  this.setState({
-	selectedPlace: props,
-	activeMarker: marker,
-	showingInfoWindow: true
-  });
-}
+		this.onMarkerClick = this.onMarkerClick.bind(this);
+		this.onMapClicked = this.onMapClicked.bind(this);
 
-onMapClicked = (props)=> {
-   if (this.state.showingInfoWindow) {
-	 this.setState({
-	   showingInfoWindow: false,
-	   activeMarker: null
-	 })
-   }
- }
+	}
+
+	onMarkerClick = (props, marker, e)=>{
+		this.setState({
+			selectedPlace: props,
+			activeMarker: marker,
+			showingInfoWindow: true
+		});
+	}
+
+	onMapClicked = (props)=> {
+		if (this.state.showingInfoWindow) {
+			this.setState({
+				showingInfoWindow: false,
+				activeMarker: null
+			})
+		}
+	}
 
 
 
-render() {
-    return (
-      <Map google={this.props.google} initialCenter={{
-          lat:32.7157,
-          lng:-117.1611
-        }}
-style={{width:"1200px", height:"600px"}}
-className='container'
- zoom={13}>
-    <Marker
-	  title={'San Diego Yacht Club'}
-      name={'SDYC'}
-      position={{lat:32.7183841, lng: -117.2292025}} />
-    <Marker
-      name={'Shelter Island Marina'}
-	  title={'Shelter Island Marina'}
-      position={{lat: 32.709925, lng: -117.231824}} />
-    <Marker />
-      <Marker
-        name={'Embarcadero Mooring field'}
-		title={'Embarcadero Mooring field'}
-        position={{lat: 32.726191, lng: -117.176331}} />
-      <Marker />
-    <Marker
-      name={'Bridge Mooring field'}
-	  title={'Bridge Mooring field'}
-      position={{lat: 32.688582, lng: -117.162332}}
-       />
-       <Marker onMouseover={this.onMouseoverMarker}
-              name={'Current location'} />
-  </Map>
-    );
-  }
+	render() {
+	    return (
+			<Map google={this.props.google} initialCenter={{
+				lat:32.7157,
+				lng:-117.1611
+	        }}
+			style={{width:"1200px", height:"600px"}}
+			className='container'
+	 		zoom={13}>
+	    		<Marker
+		  			title={'San Diego Yacht Club'}
+	      			name={'SDYC'}
+	      			position={{lat:32.7183841, lng: -117.2292025}} />
+	    		<Marker
+					name={'Shelter Island Marina'}
+					title={'Shelter Island Marina'}
+					position={{lat: 32.709925, lng: -117.231824}} />
+				<Marker />
+				<Marker
+					name={'Embarcadero Mooring field'}
+					title={'Embarcadero Mooring field'}
+					position={{lat: 32.726191, lng: -117.176331}} />
+				<Marker />
+				<Marker
+					name={'Bridge Mooring field'}
+					title={'Bridge Mooring field'}
+					position={{lat: 32.688582, lng: -117.162332}}
+				/>
+				<Marker onMouseover={this.onMouseoverMarker}
+					name={'Current location'} />
+	  		</Map>
+	    );
+	}
 }
 
 export default GoogleApiWrapper({
--- src/components/signin.js
+++ src/components/signin.js
@@ -3,39 +3,38 @@ import { Button, FormGroup, FormControl, ControlLabel, Col } from "react-bootstr
 
 
 export default class Login extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-		form: {
-	      email: "",
-	      password: ""
-	    }
-	  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			form: {
+			email: "",
+			password: ""
+			}
+		}
 	}
 
 	  validateForm() {
-	    return this.state.email.length > 0 && this.state.password.length > 0;
-	  }
+		  	return this.state.email.length > 0 && this.state.password.length > 0;
+		}
 
 	  handleChange (event) {
-		  const formState = Object.assign({}, this.state.form)
-		  formState[event.target.name] = event.target.value
-		  this.setState({form: formState})
-	  }
+			const formState = Object.assign({}, this.state.form)
+			formState[event.target.name] = event.target.value
+			this.setState({form: formState})
+		}
 
 	  handleSubmit(e) {
-		  e.preventDefault()
+		  	e.preventDefault()
 
-		  const { onSubmit } = this.props
-		  const { form } = this.state
+			const { onSubmit } = this.props
+			const { form } = this.state
 
-		  if(onSubmit) {
-			  onSubmit(form)
-		  } else {
-			  console.log("no onSubmit function was passed to signin form");
-		  }
-	  }
+			if(onSubmit) {
+				onSubmit(form)
+			} else {
+			console.log("no onSubmit function was passed to signin form");
+			}
+	  	}
 
 	  errorsFor(attribute){
         var errorString = ""
@@ -50,47 +49,49 @@ export default class Login extends Component {
         return errorString === "" ? null : errorString
     }
 
-  render() {
-	const { form } = this.state
-	const { email, password } = form
+	render() {
+		const { form } = this.state
+		const { email, password } = form
 
+	    return (
+	        <form onSubmit={this.handleSubmit.bind(this)}>
 
-    return (
-        <form onSubmit={this.handleSubmit.bind(this)}>
+	    		<FormGroup controlId="email" bsSize="large">
+	            	<ControlLabel>
+						Email
+					</ControlLabel>
+	            	<FormControl
+	              		autoFocus
+	              		type="email"
+				  		placeholder="email"
+				  		name="email"
+	              		value={email}
+	              		onChange={this.handleChange.bind(this)}
+	            	/>
+	          	</FormGroup>
 
-          <FormGroup controlId="email" bsSize="large">
+	          	<FormGroup controlId="password" bsSize="large">
+	            	<ControlLabel>
+						Password
+					</ControlLabel>
+	            	<FormControl
+						value={password}
+						onChange={this.handleChange.bind(this)}
+						placeholder="password"
+						type="password"
+						name="password"
+	            	/>
+	          	</FormGroup>
 
-            <ControlLabel>Email</ControlLabel>
-            <FormControl
-              autoFocus
-              type="email"
-			  placeholder="email"
-			  name="email"
-              value={email}
-              onChange={this.handleChange.bind(this)}
-            />
-          </FormGroup>
-          <FormGroup controlId="password" bsSize="large">
-            <ControlLabel>Password</ControlLabel>
-            <FormControl
-              value={password}
-              onChange={this.handleChange.bind(this)}
-			  placeholder="password"
-              type="password"
-			  name="password"
-            />
-          </FormGroup>
-          <Button
-            block
-			className="btn btn-success"
-            bsSize="large"
-            type="submit"
-			onClick={this.handleSubmit.bind(this)}
-          >
-            Login
-          </Button>
-        </form>
-
-    );
-  }
+	          	<Button
+					className="btn btn-success"
+		            bsSize="large"
+		            type="submit"
+					onClick={this.handleSubmit.bind(this)}
+	          	>
+	            	Login
+	          	</Button>
+	        </form>
+    	);
+	}
 }
